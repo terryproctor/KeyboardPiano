369f961e72e0b38b9dfdc7d7b47b9f01a6deb361
terryproctor <terryproctor2015@outlook.com>
2021-02-22 22:13:49 +0000

    black keys, need to position

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 document.addEventListener('keydown', e => {
     const keyPressed = e.key.toUpperCase();
-    const validKeys = ['A', 'S', 'D', 'F', 'G', 'H', 'J']
+    const validKeys = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'W', 'E', 'T', 'Y', 'U'];
+    //audio function for valid keys
     if (validKeys.includes(keyPressed)) {
         console.log(`'${keyPressed}'`);
-        // audioObject = document.getElementById(keyPressed);
-        // audioObject.play();
 
         let audioObject = document.createElement("audio");
         audioObject.src = `audio/${keyPressed}.mp3`;
         audioObject.play();
     }
+    //error alert
     else if (!validKeys.includes(keyPressed)) {
         alert('Please press a valid key!')
-    }
+    };
     })
